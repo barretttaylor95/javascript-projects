5b1da7603160b98a2e891594b92f3a43b0df39b5
Barrett Taylor <barrett.taylor95@gmail.com>
2024-06-26 12:42:02 -0500

    space mission program runs both days to mars and moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,36 @@
 // Declare and assign the variables below
 
+let shuttleName = 'Determination';
+let shuttleSpeedMPH = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621; 
+
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMPH);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof milesPerKm);
 
 // Calculate a space mission below
 
+let milestoMars = distanceToMarsKm * milesPerKm;
+let hourstoMars = distanceToMarsKm / shuttleSpeedMPH;
+let daystoMars = hourstoMars / 24;
+
 // Print the results of the space mission calculations below
+
+console.log(shuttleName + " will take " + daystoMars + " days to reach the Moon. ");
 
 // Calculate a trip to the moon below
 
+let milesToMoon = distanceToMoonKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleSpeedMPH;
+let daysToMoon = hoursToMoon / 24;
+
+
 // Print the results of the trip to the moon below
+
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon."); 
